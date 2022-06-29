d84a3697853ba82850578b1c731371dd891b072f
Kingsley Tagbo <kingsleytagbo@gmail.com>
2022-06-29 11:14:36 -0500

    update userlogin authentication

--- routes/login/authenticate.js
+++ routes/login/authenticate.js
@@ -15,7 +15,10 @@ router.post("/:siteid", async (request, response) => {
 
         const config = configs.find(c => c.privateKeyID === siteid);
         const loginResult = await login.getUserByLogin(config, body.username, body.emailaddress, params.siteid);
-        const loginUser =  (loginResult.recordset && loginResult.recordset.length > 0)? loginResult.recordset : null;
+        const loginUser =  (loginResult.recordset && loginResult.recordset.length > 0)? loginResult.recordset[0] : null;
+        if(loginUser && loginUser.AuthID){
+            await login.updateUserLoginInfo(config, body.username, body.emailaddress, params.siteid, loginUser.AuthID);
+        }
 
         const payload = {
             loginUser:loginUser, params:params, body:body, config: (config ? config.options : false)
--- routes/login/login_functions.js
+++ routes/login/login_functions.js
@@ -23,7 +23,7 @@ const LoginFunctions = {
 
         try {
             await sql.connect(config);
-            let query = ' SELECT UserID, US.UserToken, ISNULL(US.LastLoginDate, GETDATE()) AS LastLoginDate, ';
+            let query = ' SELECT US.UserToken, ISNULL(US.LastLoginDate, GETDATE()) AS LastLoginDate, ';
             query += ' DATEDIFF(mi, ISNULL(US.LastLoginDate, GETDATE()), GETDATE()) AS TimeDiffMin, NewID() AS AuthID ';
             query += ' ,WU.ITCC_WebsiteID ,US.ITCC_UserID, WS.Title AS WebsiteName' +
             ' ,RoleName = ( ' +
@@ -39,25 +39,39 @@ const LoginFunctions = {
             ' AND (WS.PrivateKeyID = ' + "'" + privateKeyID + "'" + ' ) ' +
             ') ';
 
-            const query1 = 'SELECT TOP 1 U.ITCC_UserID, U.UserName, U.UserID, U.UserToken, U.EmailAddress ' +
-            ' ,WU.ITCC_WebsiteID ITCC_WebsiteID ' +
-            ' ,RoleName = ( ' +
-            '    SELECT TOP 1 R.Name FROM ITCC_Role R(NOLOCK) JOIN ITCC_UserRole UR (NOLOCK) ON (R.ITCC_RoleID = UR.ITCC_RoleID) ' +
-            '                JOIN ITCC_Website W1 (NOLOCK) ON (UR.ITCC_WebsiteID = W1.ITCC_WebsiteID) ' +
-            '    WHERE ( (UR.ITCC_UserID = WU.ITCC_UserID) AND (W1.PrivateKeyID = ' + "'" + privateKeyID + "'" + ' ) ) ' +
-            '  ) ' +
-            ' FROM ITCC_USER U (NOLOCK) ' +
-            ' JOIN ITCC_WebsiteUser WU (NOLOCK) ON (U.ITCC_UserID = WU.ITCC_UserID) ' +
-            ' JOIN ITCC_Website W (NOLOCK) ON (WU.ITCC_WebsiteID = W.ITCC_WebsiteID) ' +
-            ' WHERE ( ' +
-            ' (RTRIM(LTRIM(LOWER(U.UserName))) = ' + "'" + username + "'" + ' ) AND (RTRIM(LTRIM(U.Password)) = ' + "'" + password + "'" + ') ' +
-            ' AND (W.PrivateKeyID = ' + "'" + privateKeyID + "'" + ' ) ' +
+            const result = await sql.query(query);
+            return result;
+
+        } catch (err) {
+            //console.log({getUserByLogin: err});
+            throw err
+        }
+    },
+
+    updateUserLoginInfo: async (config, username, password, privateKeyID, authID) => {
+        privateKeyID = privateKeyID ? String(privateKeyID).trim().toLowerCase() : privateKeyID;
+        username = username ? String(username).trim().toLowerCase() : username;
+        password = password ? String(password).trim() : password;
+
+
+        try {
+            await sql.connect(config);
+            let query = ' UPDATE ITCC_USER SET ' ;
+            query += 'UserToken = ' + "'" + authID + "'" + ', ';
+            query += 'LastLoginDate = GETDATE()';
+            query += ' FROM [ITCC_User] US (NOLOCK) JOIN [ITCC_WebsiteUser] WU (NOLOCK) ';
+            query += ' ON (US.ITCC_UserID = WU.ITCC_UserID) ';
+            query += ' JOIN [ITCC_Website] WS (NOLOCK) ON (WU.ITCC_WebsiteID = WS.ITCC_WebsiteID) ';
+            query +=   ' WHERE ( ' +
+            ' (RTRIM(LTRIM(LOWER(US.UserName))) = ' + "'" + username + "'" + ' ) AND (RTRIM(LTRIM(US.Password)) = ' + "'" + password + "'" + ') ' +
+            ' AND (WS.PrivateKeyID = ' + "'" + privateKeyID + "'" + ' ) ' +
             ') ';
 
             const result = await sql.query(query);
             return result;
+
         } catch (err) {
-            console.log({err: err});
+            //console.log({updateUserLoginInfo: err});
             throw err
         }
     }
