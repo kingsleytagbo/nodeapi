1381afae0543587588f538d326dec50496e385bc
Kingsley Tagbo <kingsleytagbo@gmail.com>
2022-07-11 17:05:19 -0500

    user management

--- routes/login/login_functions.js
+++ routes/login/login_functions.js
@@ -13,7 +13,7 @@ const LoginFunctions = {
             const roleQuery = ' RoleName = STUFF( ( ' +
             '    SELECT  ' + "'" + ','  + "'" + ' + R.Name ' + ' FROM ITCC_Role R(NOLOCK) JOIN ITCC_UserRole UR (NOLOCK) ON (R.ITCC_RoleID = UR.ITCC_RoleID) ' +
             '                JOIN ITCC_Website W1 (NOLOCK) ON (UR.ITCC_WebsiteID = W1.ITCC_WebsiteID) ' +
-            '    WHERE ( (UR.ITCC_UserID = WU.ITCC_UserID) AND (W1.PrivateKeyID = ' + "'" + privateKeyID + "'" + ' ) ) ' +
+            '    WHERE ( (UR.ITCC_UserID = WU.ITCC_UserID) AND (W1.PrivateKeyID = @PrivateKeyID) ) ' +
             '    FOR XML PATH(' + "''" + ') ) ' + ' ,1,1, ' + " '') ";
             let query = ' SELECT US.UserToken, ISNULL(US.LastLoginDate, GETDATE()) AS LastLoginDate, ';
             query += ' DATEDIFF(mi, ISNULL(US.LastLoginDate, GETDATE()), GETDATE()) AS TimeDiffMin, NewID() AS AuthID ';
@@ -24,11 +24,17 @@ const LoginFunctions = {
             query += ' ON (US.ITCC_UserID = WU.ITCC_UserID) ';
             query += ' JOIN [ITCC_Website] WS (NOLOCK) ON (WU.ITCC_WebsiteID = WS.ITCC_WebsiteID) ';
             query +=   ' WHERE ( ' +
-            ' (RTRIM(LTRIM(LOWER(US.UserName))) = ' + "'" + username + "'" + ' ) AND (RTRIM(LTRIM(US.Password)) = ' + "'" + password + "'" + ') ' +
-            ' AND (WS.PrivateKeyID = ' + "'" + privateKeyID + "'" + ' ) ' +
+            ' (RTRIM(LTRIM(LOWER(US.UserName))) = @Username) AND (RTRIM(LTRIM(US.Password)) = @Password) ' +
+            ' AND (WS.PrivateKeyID = @PrivateKeyID ) ' +
             ') ';
 
-            const result = await sql.query(query);
+            const request = new sql.Request();
+
+            request.input('PrivateKeyID', sql.UniqueIdentifier, privateKeyID);
+            request.input('AuthID', sql.UniqueIdentifier, authID);
+            request.input('Username', sql.NVarChar(64), username);
+            request.input('Password', sql.NVarChar(64), password);
+
             return result;
 
         } catch (err) {
@@ -81,12 +87,13 @@ const LoginFunctions = {
             query += ' ON (US.ITCC_UserID = WU.ITCC_UserID) ';
             query += ' JOIN [ITCC_Website] WS (NOLOCK) ON (WU.ITCC_WebsiteID = WS.ITCC_WebsiteID) ';
             query +=   ' WHERE ( ' +
-            ' ( CONVERT(VARCHAR(38),US.UserToken) = ' + "'" + authId + "'" + ' ) ' +
-            ' AND ( CONVERT(VARCHAR(38),WS.PrivateKeyID) = ' + "'" + privateKeyID + "'" + ' ) ' +
+            ' (US.UserToken = @AuthId) ' +
+            ' AND (WS.PrivateKeyID = @PrivateKeyID) ' +
             ') ';
 
-            // console.log(query);
-            const result = await sql.query(query);
+            const request = new sql.Request();
+            request.input('PrivateKeyID', sql.UniqueIdentifier, privateKeyID);
+            request.input('AuthID', sql.UniqueIdentifier, authID);
             return result;
 
         } catch (err) {
